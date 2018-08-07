98d19fec04f09cd2101850923dc17a6c46a3fdfa
ammanueladdis <ammanueladdis@gmail.com>
2018-08-07 21:56:39 +1200

    starting fresh

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">OH SHIT RELOAD SON!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App-header">         
+          <h1 className="App-title">HTTP Calls in React</h1>
+        </header>        
       </div>
     );
   }
