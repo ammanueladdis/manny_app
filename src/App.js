b8f12b5ce8768f2ec02a51c9f47f7820f115aaef
ammanueladdis <ammanueladdis@gmail.com>
2018-08-08 18:31:21 +1200

    axios installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
+import UserForm from './Components/UserForm';
+
 class App extends Component {
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    axios.get('https://api.github.com/users/${user}')
+    .then((res) => {
+      console.log(res)
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">         
           <h1 className="App-title">HTTP Calls in React</h1>
-        </header>        
+        </header>
+        <UserForm getUser={this.getUser} />        
       </div>
     );
   }
