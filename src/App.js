bcbc48fa109be7fe483b3eb7cb642c0da314b8d6
ammanueladdis <ammanueladdis@gmail.com>
2018-08-08 22:02:34 +1200

    fuck yeah finished and working

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import './App.css';
 import UserForm from './Components/UserForm';
 
 class App extends Component {
+  state = {
+    repos: null
+  }
   getUser = (e) => {
     e.preventDefault();
     const user = e.target.elements.username.value;
-    axios.get('https://api.github.com/users/${user}')
-    .then((res) => {
-      console.log(res)
-    })
+    if (user) {
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        const repos = res.data.public_repos;
+  
+        /* below line is a shorthand alternative to this.setState({ repos: repos }) - this only works because the property and variable have same name */
+        this.setState({ repos })
+         
+      })
+    }
+    else return;
   }
   render() {
     return (
@@ -19,7 +29,11 @@ class App extends Component {
         <header className="App-header">         
           <h1 className="App-title">HTTP Calls in React</h1>
         </header>
-        <UserForm getUser={this.getUser} />        
+        <UserForm getUser={this.getUser} />
+
+        {/* Sneaky ternary code below is alternative to if statement - checks if this.state.repos is true and then if true it displays the first option */} 
+        { this.state.repos ? <p>Number of repos: { this.state.repos }</p> : <p>Please enter a username in the form above</p> }
+                
       </div>
     );
   }
