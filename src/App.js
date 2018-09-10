c69c20c75a04ec7d74c53af67542e655d44fa51e
ammanueladdis <ammanueladdis@gmail.com>
2018-09-10 16:28:13 +1200

    good hustle mayunnn pg 127 keep reading.. keep hustling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Table } from 'material-ui';
+import axios from 'axios';
+
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 // Code playground
 const user = {
@@ -18,30 +21,21 @@ const user = {
 const { firstname, lastname } = user
 console.log(firstname + ' ' + lastname);
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
-onDismiss(id) {
-  const isNotId = item => item.objectID !== id;
-  const updatedHits = this.state.result.hits.filter(isNotId);
-  this.setState({
-    result: { ...this.state.result, hits: updatedHits }
-  });
-}
-
-
-
-
-
 class App extends Component {
+
+  _isMounted = false;
+
   constructor(props) {
     super(props);
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
@@ -49,65 +43,159 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
   }
 
+  onDismiss(id) {
+
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter(isNotId);
+
+    this.setState({
+      results: { 
+        ...results,
+        [searchKey]: { hits: updatedHits, page }        
+      }
+    });
+  } 
+
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({ 
+      results: { 
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)      
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }));
   }
 
   componentDidMount() {
+    this._isMounted = true;
+
     const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm)
-  
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);  
   }
 
-  onSearchSubmit() {
-    const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm)
+  componentWillUnmount() {
+    this._isMounted = false;
   }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+
+    event.preventDefault();
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
 
   render() {
-    const { searchTerm, result } = this.state;
+    const {
+      searchTerm,
+      results,
+      searchKey,
+      error
+    } = this.state;
+     
+    const page = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+
+    if (error) {
+      return <p>Something went awry oh shit dawg!</p>
+    }
+      
     return (
-      <div className="page">        ...
+      <div className="page">       
         <div className="interactions">
           <Search
             value={searchTerm}
-            onChange={this.onSearchChange}           
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}           
           >
             Search          
           </Search>        
         </div>
-        { result &&
-          <Table2
-            list={result.hits}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
+        
+        { error
+          ? <div className="interactions">
+              <p>Something went wrong.</p>
+            </div>
+          : <Table
+              list={list}            
+              onDismiss={this.onDismiss}
+            />
         }
+        
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) =>    
-  <form>
-    {children} <input
+
+const Search = ({ 
+  value,
+  onChange,
+  onSubmit,
+  children
+}) =>    
+  <form onSubmit={onSubmit}>
+    <input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <button type="submit">
+      {children}
+    </button>
   </form>
+;
 
-
-const Table2 = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
