2e4a54de4e69209a9f326dfc3e5cc7f729d25a83
ammanueladdis <ammanueladdis@gmail.com>
2018-09-13 16:10:13 +1200

    pg 136 unit testing with enzyme dawg thats hardcore!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search } from './App';
+import App, { Search, Button, Table } from './App';
 
+// testing for app mounting and a snapshot
 describe('App', () => {
 
   it('renders without crashing', () => {
@@ -20,3 +21,69 @@ describe('App', () => {
   });
 
 });
+
+// testing for Search component successful mounting and snapshot testing
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+
+// testing for Button component mounting successfully and a snapshot test
+describe('Button', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Oh yeah baby</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Button>Oh yeah baby</Button>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+});
+
+
+// testing for Table mounting successfuly / snapshot test 
+
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);    
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
