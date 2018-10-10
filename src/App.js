1847c74a0e44e2866eb69c1ba306184f390df5f8
ammanueladdis <ammanueladdis@gmail.com>
2018-10-10 20:03:13 +1300

    30 days css isn't working pg 23

--- src/App.js
+++ src/App.js
@@ -1,268 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
-import PropTypes from 'prop-types';
 
-
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = '100';
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
-
-// Code playground
-const user = {
-  firstname: 'Robin',
-  lastname: 'Wieruch'
-};
-
-const { firstname, lastname } = user
-console.log(firstname + ' ' + lastname);
 
 class App extends Component {
+	render() {
+		return (
+			<div className="notificationsFrame">
+				<div className="panel">
+					<Header />
+					<AlertBox>
+						<h1>You have pending notifications</h1>
+					</AlertBox>
+				
+				</div>
 
-  _isMounted = false;
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      results: null,
-      searchKey: '',
-      searchTerm: DEFAULT_QUERY,
-      error: null,
-    };
-
-    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
-    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
-  }
-
-  onDismiss(id) {
-
-    const { searchKey, results } = this.state;
-    const { hits, page } = results[searchKey];
-
-    const isNotId = item => item.objectID !== id;
-    const updatedHits = hits.filter(isNotId);
-
-    this.setState({
-      results: { 
-        ...results,
-        [searchKey]: { hits: updatedHits, page }        
-      }
-    });
-  } 
-
-  setSearchTopStories(result) {
-    const { hits, page } = result;
-    const { searchKey, results } = this.state;
-
-    const oldHits = results && results[searchKey]
-      ? results[searchKey].hits
-      : [];
-
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
-
-    this.setState({ 
-      results: { 
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      }
-    });
-  }
-
-  needsToSearchTopStories(searchTerm) {
-    return !this.state.results[searchTerm];
-  }
-
-  fetchSearchTopStories(searchTerm, page = 0) {
-    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)      
-      .then(result => this._isMounted && this.setSearchTopStories(result.data))
-      .catch(error => this._isMounted && this.setState({ error }));
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-
-    const { searchTerm } = this.state;
-    this.setState({ searchKey: searchTerm });
-    this.fetchSearchTopStories(searchTerm);  
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  onSearchSubmit(event) {
-    const { searchTerm } = this.state;
-    this.setState({ searchKey: searchTerm });
-
-    if (this.needsToSearchTopStories(searchTerm)) {
-      this.fetchSearchTopStories(searchTerm);
-    }
-
-    event.preventDefault();
-  }
-
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
-  }
+			</div>
+		);
+	}
+}
 
 
-  render() {
-    const {
-      searchTerm,
-      results,
-      searchKey,
-      error
-    } = this.state;
-     
-    const page = (
-      results &&
-      results[searchKey] &&
-      results[searchKey].page
-    ) || 0;
 
-    const list = (
-      results &&
-      results[searchKey] &&
-      results[searchKey].hits
-    ) || [];
+class Header extends Component {
+	render() {
+		return (
+			<div className="header">
+				<div className="fa fa-more"></div>
 
-    if (error) {
-      return <p>Something went awry oh shit dawg!</p>
-    }
-      
-    return (
-      <div className="page">       
-        <div className="interactions">
-          <Search
-            value={searchTerm}
-            onChange={this.onSearchChange}
-            onSubmit={this.onSearchSubmit}           
-          >
-            Search          
-          </Search>        
-        </div>
-        
-        { error
-          ? <div className="interactions">
-              <p>Something went wrong.</p>
-            </div>
-          : <Table
-              list={list}            
-              onDismiss={this.onDismiss}
-            />
-        }
-        
-        <div className="interactions">
-          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
-            More
-          </Button>
-        </div>
-      </div>
-    );
+				<span className="title">Timeline</span>
+
+				<input
+					type="text"
+					className="searchInput"
+					placeholder="Search ..." />
+
+				<div className="fa fa-search searchIcon"></div>				
+			</div>
+		);
+	}
+}
+
+
+ 
+class AlertBox extends React.Component {
+  render () {
+    return <div className='alert-box'>
+      {this.props.children}
+    </div>
   }
 }
 
 
-const Search = ({ 
-  value,
-  onChange,
-  onSubmit,
-  children
-}) =>    
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
-;
-
-// Table ES6 class and table proptypes below
-const Table = ({ list, onDismiss }) =>
-  <div className="table">
-    {list.map(item =>
-      <div key={item.objectID} className="table-row">
-        <span style={{ width: '40%' }}>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span style={{ width: '30%' }}>
-          {item.author}
-        </span>
-        <span style={{ width: '10%' }}>
-          {item.num_comments}
-        </span>
-        <span>
-          {item.points}
-        </span>
-        <span>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-            className="button-inline"          
-          >
-            Dismiss          
-          </Button>
-        </span>
-      </div>   
-    )}  
-  </div>
-
-Table.propTypes = {
-  list: PropTypes.arrayOf(
-    PropTypes.shape({
-      objectID: PropTypes.string.isRequired,
-      author: PropTypes.string,
-      url: Proptypes.string,
-      num_comments: PropTypes.number,
-      points: PropTypes.number,
-    })
-  ).isRequired,
-  onDismiss: PropTypes.func.isRequired,
-};
-
-
-
-// Button and Button proptypes
-const Button = ({
-  onClick,
-  classname = '',
-  children,
-}) =>
-  <button
-    onClick={onClick}
-    className={className}
-    type="button"
-  >
-    {children}
-  </button>
-
-Button.PropTypes = {
-  onClick: PropTypes.func.isRequired,
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired,
-};
-
 
 export default App;
 
-export {
-  Button,
-  Search,
-  Table
-};
+
+
+
